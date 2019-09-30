18a902571a5a014fe069b0868dd867476023d52c
Emelie <mehadhbi@hotmail.com>
2019-09-30 08:55:17 +0200

    Initial menu and exit functionality

--- Program.cs
+++ Program.cs
@@ -7,6 +7,38 @@ namespace Resturant
     {
         static void Main(string[] args)
         {
+
+            bool shouldNotExit = true;
+
+            while (shouldNotExit)
+            {
+                Clear();
+
+                WriteLine("1. Add order");
+                WriteLine("2. List orders");
+                WriteLine("3. Exit");
+
+                ConsoleKeyInfo keyPressed = ReadKey();
+
+                switch (keyPressed.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+
+                        break;
+
+                    case ConsoleKey.D3:
+                        shouldNotExit = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
         }
     }
 }
